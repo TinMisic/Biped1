74322b6568c3886664264eec4235b7dac40c4b8f
Tin Misic <tin@pop-os.localdomain>
2022-08-31 12:17:33 +0200

    auto_shutdown and buzzer test

---
+++ python/auto_shutdown.py
@@ -0,0 +1,28 @@
+import time
+from subprocess import call
+
+total_t = 310 # change to value from timer.txt
+wait_t = 300 # 5 min wait before auto shutdown
+
+t = total_t - wait_t #seconds until auto_shutdown
+time.sleep(t)
+
+# buzz tune to inform user of empty battery and auto_shutdown
+
+from pymata4 import pymata4
+board = pymata4.Pymata4()
+TONE_PIN = 9
+notes = {"c":523,"d":587,"e":659,"f":698,"g":784,"a":880,"h":988,"c2":1047} #B E G A
+tune = ["b","e","g","a"]
+
+for note in tune:
+    freq = notes[note]
+    board.play_tone(TONE_PIN, freq, 500)
+    time.sleep(0.5)
+
+board.shutdown()
+
+time.sleep(wait_t)
+
+# shutdown
+call("sudo poweroff", shell=True)
